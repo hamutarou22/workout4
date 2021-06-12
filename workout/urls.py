fcdf3cb4b4f0aadd66e6b9907909fb712d3b4162
hamutarou22 <hamutarou22@gmail.com>
2021-06-13 03:31:19 +0900

    ss

--- workout/urls.py
+++ workout/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('workout/', include('workoutapp.urls')),
+    path('admin/', admin.site.urls),
 ]
