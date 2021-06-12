a1b0c638d8c2c17b4e866ca11650dd7448cfb41b
hamutarou22 <hamutarou22@gmail.com>
2021-06-13 03:57:09 +0900

    SITE_ID ついか

--- workout/settings.py
+++ workout/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sites',
 ]
 
+SITE_ID=1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
